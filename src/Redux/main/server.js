5512d38038095769db8adff9b1435e9c48a5378b
Ahsan-Mahfuz <ahsanmahfuz0@gmail.com>
2026-02-25 14:52:19 +0600

    updae

--- src/Redux/main/server.js
+++ src/Redux/main/server.js
@@ -1,2 +1,2 @@
-export const url = `http://10.10.20.44:8003`;
-export const image_url = `http://10.10.20.44:8003`;
+export const url = `http://13.50.99.165:8003`;
+export const image_url = `http://13.50.99.165:8003`;
